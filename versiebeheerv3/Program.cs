54c1da0d3abda411a8e55dd8b37070ceadc21c7f
Asus <aslansemih005@gmail.com>
2023-06-16 15:20:44 +0200

    dit was de verandering

--- versiebeheerv3/Program.cs
+++ versiebeheerv3/Program.cs
@@ -6,7 +6,7 @@ namespace versiebeheerv3
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("hallo mijn naam is semih en dit is mijn git opdracht");
+            Console.WriteLine("dit is mijn verandering");
         }
     }
 }
