fca5568ccebd3fd094bc2ead464bc5f4d1858dfd
Asus <aslansemih005@gmail.com>
2023-06-16 14:03:46 +0200

    dit is een mooie test

---
+++ versiebeheerv3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace versiebeheerv3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hallo mijn naam is semih en dit is mijn git opdracht");
+            hllo
+            
+        }
+    }
+}
